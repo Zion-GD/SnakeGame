9f5db9f8e90e6b0239b057c4ef3249596ad93872
hzahzahza <zianh@usc.edu>
2025-06-11 23:42:57 +0800

    UI component and event

--- Class/Option.cs
+++ Class/Option.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SnakeGame.Class
 {
-    class Option
+    class Option : IUIComponent
     {
         private string[] Options;
+        private Vector2 pos;
         private ConsoleColor selectColor, unselectColor;
         private int LineSpace;
         public int currIndex;
 
-        public Option(string[] options, ConsoleColor selectColor, ConsoleColor unselectColor, int space )
+        public Option(string[] options, ConsoleColor selectColor, ConsoleColor unselectColor, Vector2 pos, int space )
         {
             Options = options;
+            this.pos = pos;
             this.selectColor = selectColor;
             this.unselectColor = unselectColor;
             this.LineSpace = space;
@@ -30,23 +33,19 @@ namespace SnakeGame.Class
             set { Options[index] = value; }
         }
 
-        public void DrawOption(Vector2 pos)
+        public void Refresh()
         {
             for(int i = 0; i < Options.Length; ++i)
-            {
-                //Set option color
-                if(i == currIndex) Console.ForegroundColor = selectColor;
-                else Console.ForegroundColor = unselectColor;
-                
-                Console.SetCursorPosition(pos.x - Options[i].Length / 2, pos.y - LineSpace * i);
-                Console.WriteLine(Options[i]);
+            {              
+                UITools.DrawText(Options[i], new Vector2(pos.x - Options[i].Length / 2, pos.y - LineSpace * i),
+                    i == currIndex? selectColor : unselectColor);
             }
         }
 
         public void SelectOption(int dir)
         {
             currIndex = (currIndex + dir + Options.Length) % Options.Length;
-            
+            Refresh();
         }
     }
 }
---
+++ Class/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame.Class
+{
+    class UIManager
+    {
+        private static UIManager instance = new UIManager();
+        public static UIManager Instance
+        {
+            get { return instance; }
+        }
+        private List<IUIComponent> components = new List<IUIComponent>();
+
+        public event Action onRefresh;
+
+        public void ComponentRegister(IUIComponent uic)
+        {
+            components.Add(uic);
+            onRefresh += uic.Refresh;
+        }
+
+        public void RefreshAll()
+        {
+            onRefresh?.Invoke();
+        }
+    }
+}
